d2a7b520d19168b944924953b050370a7dc8a44c
emorris2 <emaemorris@yahoo.com>
2020-11-16 16:24:21 -0700

    Assignment

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp","stapler", "computer",  "computer"];
+
+var counter = 0;
+
+for( var i = 0; i < officeItems.length; i++) {
+    //console.log(officeItems[i]);
+    
+    if( officeItems[i] === "computer" ) {
+        //counter = counter + 1;
+        counter += 1;
+    }
+}
+
+console.log( counter );
+
+
 var peopleWhoWantToSeeMadMaxFuryRoad = [
     {
       name: "Mike",
